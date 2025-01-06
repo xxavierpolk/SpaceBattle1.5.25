62024984efcdab9bccd08faba86ef7abe0295ffe
Xxavier Polk <xxavier@xxavierpolk.com>
2025-01-06 01:36:25 -0500

    Log of Generating Alien Fleet

--- app.js
+++ app.js
@@ -24,18 +24,24 @@ class SpaceShip {
 const myPlayer = new SpaceShip("USS ASSEMBLY", 20, 5, 0.7);
 
 
-// Alien Gang
+// Alien Gang with a function
 function generateAlienGang(numOfShips) {
     const alienGang = [];
     for (let i = 0; i < numOfShips; i++) {
         const hull = Math.floor(Math.random() * (6 - 3 + 1) + 3);
         const firepower = Math.floor(Math.random() * (4 - 2 + 1) + 2);
         const accuracy = Math.random() * (0.8 - 0.6) + 0.6;
-        alienShips.push(new SpaceShip('Alien Ship ${i + 1}', hull, firepower, accuracy));
+        alienGang.push(new SpaceShip('Alien Ship{i  $+ 1}', hull, firepower, accuracy));
     }
     return alienGang;
 
 }
+
+// Creating the Alien Gang Fleet
+
+const alienFleet = generateAlienGang(6)
+console.log("Alien Fleet", alienFleet)
+
 
 // Game Round 
 
